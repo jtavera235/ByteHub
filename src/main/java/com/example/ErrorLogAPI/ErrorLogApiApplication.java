730852b62bbf917ea6584da41f621e60d56ec44d
Juan Tavera <jtavera235@Juans-MacBook-Pro.local>
2020-06-17 18:35:34 -0400

    Fixed component scan part 2

--- src/main/java/com/example/ErrorLogAPI/ErrorLogApiApplication.java
+++ src/main/java/com/example/ErrorLogAPI/ErrorLogApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.example.ErrorLogAPI.controller"})
+@ComponentScan({"com.example.ErrorLogAPI.controllers"})
 public class ErrorLogApiApplication implements CommandLineRunner {
 
 
